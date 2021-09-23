1b53ce99a38542f6d37d4c04c89a0ebc0a4202a9
René Rhéaume <rene.rheaume@gmail.com>
2021-09-23 07:03:44 -0400

    Normalized AssemblyInfo files with regions.

--- HqxSharpTest/Properties/AssemblyInfo.cs
+++ HqxSharpTest/Properties/AssemblyInfo.cs
@@ -1,27 +1,37 @@
+using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+#region General Information
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HqxSharpTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+#endregion
+[assembly: AssemblyTitle("hqxSharp Test")]
+[assembly: AssemblyDescription("Demonstration and benchmark program for hqxSharp")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HqxSharpTest")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("hqxSharp")]
+[assembly: AssemblyCopyright("Copyright (C) 2020-2021 René Rhéaume. This program is licensed under the GNU Lesser General Public License version 3 or any later version.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#region Interoperability Attributes
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The GUID is for the ID of the typelib if this project is exposed to COM
+#endregion
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
 [assembly: Guid("edaa07f5-b120-47e9-8cda-7b46cc80b7ed")]
 
+#region Version Information
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +42,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#endregion
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- hqxSharp/Properties/AssemblyInfo.cs
+++ hqxSharp/Properties/AssemblyInfo.cs
@@ -1,26 +1,37 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+#region General Information
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hqxSharp")]
+#endregion
+[assembly: AssemblyTitle("hqxSharp Library")]
 [assembly: AssemblyDescription("hqxSharp is a C# port of hqx, a fast, high-quality magnification filter designed for pixel art.")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (C) 2003 Maxim Stepin (maxst@hiend3d.com), Copyright (C) 2010 Cameron Zemek (grom@zeminvaders.net), Copyright (C) 2011 Tamme Schichler (tamme.schichler@googlemail.com). This program is licensed under the GNU Lesser General Public License version 3 or any later version.")]
+[assembly: AssemblyProduct("hqxSharp")]
+[assembly: AssemblyCopyright("Copyright (C) 2003 Maxim Stepin (maxst@hiend3d.com), Copyright (C) 2010 Cameron Zemek (grom@zeminvaders.net), Copyright (C) 2011 Tamme Schichler (tamme.schichler@googlemail.com). Copyright (C) 2013, 2020-2021 René Rhéaume. This program is licensed under the GNU Lesser General Public License version 3 or any later version.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+#region Interoperability Attributes
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The GUID is for the ID of the typelib if this project is exposed to COM
+#endregion
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
 [assembly: Guid("e23ca469-25ad-4601-83d0-fcefb0193147")]
 
+#region Version Information
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -31,5 +42,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+#endregion
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
