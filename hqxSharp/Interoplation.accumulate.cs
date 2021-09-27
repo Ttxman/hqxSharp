0b666e9326bc7338f9cd972ce60fd55bffb83a6f
René Rhéaume <rene.rheaume@gmail.com>
2021-09-27 05:18:31 -0400

    Added method usage counts comment for targeting future optimizations.

--- hqxSharp/Interoplation.accumulate.cs
+++ hqxSharp/Interoplation.accumulate.cs
@@ -28,51 +28,78 @@ namespace hqx
 	/// </summary>
 	internal static class Interpolation
 	{
+		// 1572 calls :  288 in 2x, 756 in 3x,  528 in 4x
 		public static uint Mix3To1(uint c1, uint c2)
 		{
 			return MixColours(3, 1, c1, c2);
 		}
 
+		//  676 calls :  316 in 2x, 180 in 3x,  180 in 4x
 		public static uint Mix2To1To1(uint c1, uint c2, uint c3)
 		{
 			return MixColours(2, 1, 1, c1, c2, c3);
 		}
 
+		//  760 calls : NONE in 2x, 120 in 3x,  640 in 4x
 		public static uint Mix7To1(uint c1, uint c2)
 		{
 			return MixColours(7, 1, c1, c2);
 		}
 
+		//   72 calls : NONE in 2x,  72 in 3x, NONE in 4x
 		public static uint Mix2To7To7(uint c1, uint c2, uint c3)
 		{
 			return MixColours(2, 7, 7, c1, c2, c3);
 		}
 
+		/*
+		From 2xSaI source
+		In 15bpp,
+			colorMask		is 0x7BDE or binary 0111 1011 1101 1110, i.e. 3 sequences of 11110
+			lowPixelMask	is 0x0421 or binary 0000 0100 0010 0001, i.e. 3 sequences of 00001, complement of the above
+
+		The function below works with two 15/16 bits pixels packed in a 32 bits variable
+		static inline uint32 INTERPOLATE(uint32 A, uint32 B)
+		{
+			if (A != B) {
+				return ( ((A & colorMask) >> 1) + ((B & colorMask) >> 1) + (A & B & lowPixelMask) );
+			} else {
+				return A;
+			}
+		}
+		*/
+
+		//  264 calls : NONE in 2x,  24 in 3x,  240 in 4x
 		public static uint MixEven(uint c1, uint c2)
 		{
 			return MixColours(1, 1, c1, c2);
 		}
 
+		//  344 calls :   24 in 2x, NONE in 3x, 320 in 4x
 		public static uint Mix5To2To1(uint c1, uint c2, uint c3)
 		{
 			return MixColours(5, 2, 1, c1, c2, c3);
 		}
 
+		//  152 calls :   52 in 2x, NONE in 3x, 100 in 4x
 		public static uint Mix6To1To1(uint c1, uint c2, uint c3)
 		{
 			return MixColours(6, 1, 1, c1, c2, c3);
 		}
 
+		//  664 calls : NONE in 2x, NONE in 3x, 664 in 4x
 		public static uint Mix5To3(uint c1, uint c2)
 		{
 			return MixColours(5, 3, c1, c2);
 		}
 
+		//   24 calls :   24 in 2x, NONE in 3x, NONE in 4x
 		public static uint Mix2To3To3(uint c1, uint c2, uint c3)
 		{
 			return MixColours(2, 3, 3, c1, c2, c3);
 		}
 
+		//   28 calls :   28 in 2x, NONE in 3x, NONE in 4x
 		public static uint Mix14To1To1(uint c1, uint c2, uint c3)
 		{
 			return MixColours(14, 1, 1, c1, c2, c3);
