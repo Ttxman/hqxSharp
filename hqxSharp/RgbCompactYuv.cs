33d3d7de2b4385885b44f30579281852d1803b00
René Rhéaume <rene.rheaume@gmail.com>
2021-09-28 05:39:40 -0400

    Separated YUV value computation from retrieval, to facilitate future profiling

--- hqxSharp/RgbCompactYuv.cs
+++ hqxSharp/RgbCompactYuv.cs
@@ -43,26 +43,27 @@ namespace hqx
 
 			rgb &= RgbMask;
 			int yuv;
-			if (s_dicRgbYuv.TryGetValue(rgb, out yuv)) {
-				return yuv;
-			} else {
-				var r = ((int)rgb & 0xff0000) >> 16;
-				var g = ((int)rgb & 0x00ff00) >> 8;
-				var b = (int)rgb & 0x0000ff;
+			return s_dicRgbYuv.TryGetValue(rgb, out yuv) ? yuv : ComputeYuv(rgb);
+		}
 
-				// To prevent a round-trip to the FPU, some fixed-point math is used.
-				// RGB values go from 0 to 255. If we were multiplying them by
-				// 65793, they would go from 0 to 16777215 (2^24-1)
-				// After scaling coefficients (found in RgbYuv), a right shift 
-				// is performed to bring numbers back to a 0..255 scale.
-				var y = ((+19672 * r) + (38620 * g) + (7500 * b)) >> 16;
-				var u = (((-11119 * r) - (21777 * g) + (32896 * b)) >> 16) + 128;
-				var v = (((+32896 * r) - (27567 * g) - (5329 * b)) >> 16) + 128;
+		private static int ComputeYuv(uint rgb)
+		{
+			var r = ((int)rgb & 0xff0000) >> 16;
+			var g = ((int)rgb & 0x00ff00) >> 8;
+			var b = ((int)rgb & 0x0000ff);
 
-				yuv = (y << 16) | (u << 8) | v;
-				s_dicRgbYuv.Add(rgb, yuv);
-				return yuv;
-			}
+			// To prevent a round-trip to the FPU, some fixed-point math is used.
+			// RGB values go from 0 to 255. If we were multiplying them by
+			// 65793, they would go from 0 to 16777215 (2^24-1)
+			// After scaling coefficients (found in RgbYuv), a right shift
+			// is performed to bring numbers back to a 0..255 scale.
+			var y = (((+19672 * r) + (38620 * g) + (7500 * b)) >> 16);
+			var u = (((-11119 * r) - (21777 * g) + (32896 * b)) >> 16) + 128;
+			var v = (((+32896 * r) - (27567 * g) - (5329 * b)) >> 16) + 128;
+
+			var yuv = (y << 16) | (u << 8) | v;
+			s_dicRgbYuv.Add(rgb, yuv);
+			return yuv;
 		}
 
 		public static void UnloadLookupTable()
