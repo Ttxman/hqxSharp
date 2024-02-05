d91e886ccda5df6fae1318391bc0c1a5f419591d
René Rhéaume <repzilon@users.noreply.github.com>
2024-02-04 23:21:01 -0500

    Corrected a typo

--- HqxTerser/Program.cs
+++ HqxTerser/Program.cs
@@ -32,7 +32,7 @@ namespace HqxTerser
 				var strHqxCopy = strHqxOrig;
 
 				strHqxCopy = Regex.Replace(strHqxCopy, @"\*dp = Interpolation.Mix2To1To1\(w\[4\], w\[(\d)\], w\[(\d)\]\);\s+\*\(dp \+ 1\) = Interpolation.Mix2To1To1\(w\[4\], w\[(\d)\], w\[(\d)\]\);\s+\*\(dp \+ dpL\) = Interpolation.Mix2To1To1\(w\[4\], w\[(\d)\], w\[(\d)\]\);\s+\*\(dp \+ dpL \+ 1\) = Interpolation.Mix2To1To1\(w\[4\], w\[(\d)\], w\[(\d)\]\);", @"All4Mix2To1To1(dp, dpL, w, $1, $2, $3, $4, $5, $6, $7, $8);");
-				strHqxCopy = Regex.Replace(strHqxCopy, @"(\W)w[[]4[]](\W)", "$1middle$2").Replace("middle = middle;", "middle = w[4];"); ;
+				strHqxCopy = Regex.Replace(strHqxCopy, @"(\W)w[[]4[]](\W)", "$1middle$2").Replace("middle = middle;", "middle = w[4];");
 
 				if (strHqxCopy != strHqxOrig) {
 					File.WriteAllText(strFilename, strHqxCopy);
