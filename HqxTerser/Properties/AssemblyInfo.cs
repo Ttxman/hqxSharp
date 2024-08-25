4a4636da08d97abe164422314491c30b37fe5416
Ttxman <seps.ladislav@seznam.cz>
2024-08-25 03:01:00 +0200

    net8 upgrade

--- HqxTerser/Properties/AssemblyInfo.cs
+++ HqxTerser/Properties/AssemblyInfo.cs
@@ -1,22 +1,6 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-#region General Information
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-#endregion
-[assembly: AssemblyTitle("HqxTerser")]
-[assembly: AssemblyDescription("Utility to reduce hqxSharp source code size by finding ans replacing patterns")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hqxSharp")]
-[assembly: AssemblyCopyright("Copyright (C) 2020-2021 René Rhéaume. This program is licensed under the GNU Lesser General Public License version 3 or any later version.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,18 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("4c1bbe95-1124-4d88-9a92-e9ade8c64b19")]
-
-#region Version Information
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-#endregion
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- hqxSharp/Properties/AssemblyInfo.cs
+++ hqxSharp/Properties/AssemblyInfo.cs
@@ -1,22 +1,6 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-#region General Information
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-#endregion
-[assembly: AssemblyTitle("hqxSharp Library")]
-[assembly: AssemblyDescription("hqxSharp is a C# port of hqx, a fast, high-quality magnification filter designed for pixel art.")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hqxSharp")]
-[assembly: AssemblyCopyright("Copyright (C) 2003 Maxim Stepin (maxst@hiend3d.com), Copyright (C) 2010 Cameron Zemek (grom@zeminvaders.net), Copyright (C) 2011 Tamme Schichler (tamme.schichler@googlemail.com). Copyright (C) 2013, 2020-2021 René Rhéaume. This program is licensed under the GNU Lesser General Public License version 3 or any later version.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,18 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("e23ca469-25ad-4601-83d0-fcefb0193147")]
-
-#region Version Information
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-#endregion
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
